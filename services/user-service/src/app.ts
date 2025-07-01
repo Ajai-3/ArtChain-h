4be9d1db5dfb23398a104b9b03196d80ab6b2ca0
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 18:49:19 +0530

    Add cookie-parser middleware to the Express app

--- services/user-service/src/app.ts
+++ services/user-service/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from 'cookie-parser';
+
 import userRouter from "../src/interfaces/routes/user.routes";
 
 
 const app = express();
 
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
