81d568589318d398bd6b4e86170a32afa489c8ec
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 12:35:41 +0530

    Refactor User entity to allow nullable fields for profile images, bio, and country

--- services/user-service/src/interfaces/routes/user.routes.ts
+++ services/user-service/src/interfaces/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
-const router = express.Router();
-
 import { registerUser } from "../controllers/UserController";
+
+const router = express.Router();
 
 router.post("/register", registerUser);
 
