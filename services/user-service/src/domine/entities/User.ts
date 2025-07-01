b96aa6d0126ea4b140256144bbcda83e655256c0
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 10:31:47 +0530

    Refactor User entity to include _id and correct country property name

--- services/user-service/src/domine/entities/User.ts
+++ services/user-service/src/domine/entities/User.ts
@@ -1,20 +1,20 @@
 export class User {
   constructor(
-    public _id: string,
+
     public name: string,
     public username: string,
     public email: string,
     public phone: number,
     public password: string,
-    public role: "user" | "artist" | "admin",
-    public plan: "free" | "pro" | "pro_plus",
-    public status: "banned" | "suspended" | "active" | "deleted",
     public isVerified: boolean,
     public profileImage: string,
     public bannerImage: string,
     public backroundImage: string,
     public bio: string,
-    public contry: string,
+    public country: string,
+    public role: "user" | "artist" | "admin",
+    public plan: "free" | "pro" | "pro_plus",
+    public status: "banned" | "suspended" | "active" | "deleted",
     public createdAt: Date,
     public updatedAt: Date
   ) {}
