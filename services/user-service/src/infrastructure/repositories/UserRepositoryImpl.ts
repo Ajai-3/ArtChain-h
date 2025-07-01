ae98d9f9b622c64069d848808093ab2bf4136c7f
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 10:31:56 +0530

    Add UserRepositoryImpl class implementing IUserRepository interface

---
+++ services/user-service/src/infrastructure/repositories/UserRepositoryImpl.ts
@@ -0,0 +1,6 @@
+import { User } from "../../domine/entities/User";
+import { IUserRepository } from "../../domine/repositories/IUserRepositories";
+
+export class UserRepositoryImpl implements IUserRepository {
+
+}
