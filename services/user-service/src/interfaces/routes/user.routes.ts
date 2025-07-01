bb24ff56a51333484598ce2c3add21ab4006e9a4
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 18:49:31 +0530

    Add refresh token route to user routes

--- services/user-service/src/interfaces/routes/user.routes.ts
+++ services/user-service/src/interfaces/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { loginUser, registerUser } from "../controllers/AuthController";
+import { loginUser, refreshToken, registerUser } from "../controllers/AuthController";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser)
+router.get("/refresh-token", refreshToken)
 
 export default router;
