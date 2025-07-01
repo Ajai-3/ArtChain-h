eca7a6465b10885b69fa385fa8b20319485be1c3
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 17:46:25 +0530

    Add AuthenticationError class for error handling in authentication

---
+++ services/user-service/src/errors/AuthenticationError.ts
@@ -0,0 +1,10 @@
+
+//# Error handling class
+export class AuthenticationError  extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "AuthenticationError";
+
+    Object.setPrototypeOf(this, AuthenticationError.prototype);
+  }
+}
