a4234e9f4680c590c7fb8473d227c56988592b29
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 10:31:41 +0530

    Implement RegisterUserUseCase for user registration with validation for existing username and email

--- services/user-service/src/application/user/RegisterUserUseCase.ts
+++ services/user-service/src/application/user/RegisterUserUseCase.ts
@@ -1,1 +1,44 @@
 import { IUserRepository } from "../../domine/repositories/IUserRepositories";
+
+export class RegisterUserUseCase {
+  constructor(private userRepo: IUserRepository) {}
+
+  async execute(
+    name: string,
+    username: string,
+    email: string,
+    phone: number,
+    password: string
+  ) {
+    const existingUserByUsername = await this.userRepo.findOneByUsername(
+      username
+    );
+    if (existingUserByUsername) {
+      throw new Error("Username already exists");
+    }
+    const existingUserByEmail = await this.userRepo.findOneByEmail(email);
+    if (existingUserByEmail) {
+      throw new Error("Email already exists");
+    }
+
+    const user = await this.userRepo.create({
+      name,
+      username,
+      email,
+      phone,
+      password,
+      isVerified: false,
+      profileImage: "",
+      bannerImage: "",
+      backroundImage: "",
+      bio: "",
+      country: "",
+      role: "user",
+      plan: "free",
+      status: "active",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    return user;
+  }
+}
