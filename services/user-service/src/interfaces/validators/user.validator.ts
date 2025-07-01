ce6a4b3a1798fb33a248e1c91ef5bacc1516d899
Ajai-3 <ajaipjayan@gmail.com>
2025-07-01 11:37:47 +0530

    Add user registration schema validation using Zod

--- services/user-service/src/interfaces/validators/user.validator.ts
+++ services/user-service/src/interfaces/validators/user.validator.ts
@@ -1,1 +1,36 @@
-import zod from "zod";
+import { z } from "zod";
+
+//#==================================================================================================================
+//# REGISTER USER SCHEMA VALIDATION
+//#==================================================================================================================
+export const registerUserSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name too short" })
+    .max(20, { message: "Name too long" })
+    .regex(/^[A-Za-z ]+$/, { message: "Only letters & spaces" }),
+
+  username: z
+    .string()
+    .min(3, { message: "Username too short" })
+    .max(20, { message: "Username too long" })
+    .regex(/^[a-zA-Z0-9_]+$/, { message: "Use letters, numbers, _" }),
+
+  email: z
+    .string()
+    .email({ message: "Invalid email" }),
+
+  password: z
+    .string()
+    .min(8, { message: "Min 8 chars" })
+    .refine(
+      (val) =>
+        /[A-Z]/.test(val) &&
+        /[a-z]/.test(val) &&
+        /[0-9]/.test(val) &&
+        /[^A-Za-z0-9]/.test(val),
+      {
+        message: "Must include A-Z, a-z, 0-9, symbol",
+      }
+    ),
+});
