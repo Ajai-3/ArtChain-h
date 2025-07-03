4b4eda636c45bdff90b87feb834461f1ef598219
Ajai-3 <ajaipjayan@gmail.com>
2025-07-03 10:58:59 +0530

    Implement basic Express server setup in app.ts and server.ts

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -1,1 +1,13 @@
-import express from 'express';
+import app from "./app";
+import http from "http";
+import { config } from "./config/env";
+
+const PORT = config.port;
+
+
+const server = http.createServer(app);
+
+
+server.listen(PORT, () => {
+    console.log(`API gateway running at http://localhost:${PORT}`);
+})
