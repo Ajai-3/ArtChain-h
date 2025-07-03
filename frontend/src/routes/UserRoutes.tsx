84f47592a86b3a5df6452c83e4e44d656a8bde8f
Ajai-3 <ajaipjayan@gmail.com>
2025-07-03 23:02:30 +0530

    feat: add login route to UserRoutes for authentication

--- frontend/src/routes/UserRoutes.tsx
+++ frontend/src/routes/UserRoutes.tsx
@@ -2,11 +2,13 @@ import { Route } from "react-router-dom";
 import UserLayout from "../layouts/UserLayout";
 import Home from "../features/user/pages/Home";
 import Profile from "../features/user/pages/Profile";
+import Auth from "../features/user/components/auth/Auth";
 
 const UserRoutes = (
   <Route path="/" element={<UserLayout />}>
     <Route index element={<Home />} />
     <Route path="profile" element={<Profile />} />
+    <Route path="login" element={<Auth />} />
   </Route>
 );
 
