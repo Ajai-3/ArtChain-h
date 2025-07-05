0755a157571458e910c428d1e1e4414be1f352ed
Ajai-3 <ajaipjayan@gmail.com>
2025-07-05 13:44:58 +0530

    fix: api gtway rout forwarding ishue solved

--- api-gateway/src/gateway/main.route.ts
+++ api-gateway/src/gateway/main.route.ts
@@ -3,7 +3,9 @@ import { userProxy, adminProxy } from "./services/main.proxy"
 
 const router = Router()
 
-router.use("/api/v1/users", userProxy)
-router.use("/api/v1/admin", adminProxy)
+console.log("hello")
+
+router.use("/", userProxy)
+router.use("/", adminProxy)
 
 export default router
--- api-gateway/src/gateway/services/main.proxy.ts
+++ api-gateway/src/gateway/services/main.proxy.ts
@@ -1,11 +1,23 @@
 import { config } from "../../config/env";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import { createProxyMiddleware, Options } from "http-proxy-middleware";
+
+console.log("User service target:", config.services.main)
 
 export const userProxy = createProxyMiddleware({
-  target: config.services.main, 
+  target: config.services.main,
   changeOrigin: true,
-  pathRewrite: { "^/api/v1/users": "/api/v1/users" }
-});
+  pathRewrite: { 
+    "^/api/v1/users": "/api/v1/users"
+  },
+  on: { 
+    proxyReq: (proxyReq, req, res) => {
+      console.log(`[PROXY] Forwarding ${req.method} ${req.url} -> ${proxyReq.path}`);
+    },
+    error: (err, req, res) => {
+      console.error('[PROXY ERROR]', err);
+    }
+  }
+} as Options); 
 
 export const adminProxy = createProxyMiddleware({
   target: config.services.main, 
