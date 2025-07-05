a53979ba68566fc635da1ca4ae35d58388ee9134
Ajai-3 <ajaipjayan@gmail.com>
2025-07-05 18:48:56 +0530

    reset password apge created

--- frontend/src/routes/UserRoutes.tsx
+++ frontend/src/routes/UserRoutes.tsx
@@ -3,9 +3,13 @@ import UserLayout from "../layouts/UserLayout";
 import Home from "../features/user/pages/Home";
 import Profile from "../features/user/pages/Profile";
 import Auth from "../features/user/components/auth/Auth";
+import ResetPassword from "../features/user/components/auth/ResetPassword";
+import SignupPassword from "../features/user/components/auth/SignupPassword";
 
 const UserRoutes = [
   <Route path="/login" element={<Auth />} key="auth" />,
+  <Route path="/reset-password" element={<ResetPassword />} />,
+  <Route path="/verify" element={<SignupPassword />} />,
 
   <Route path="/" element={<UserLayout />} key="user-layout">
     <Route index element={<Home />} />
