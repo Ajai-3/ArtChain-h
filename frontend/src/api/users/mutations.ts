34aa4626e35410fe6fe49b11076af5c4a5f377d5
Ajai-3 <ajaipjayan@gmail.com>
2025-07-05 14:52:37 +0530

    feat: sign up mutation added

--- frontend/src/api/users/mutations.ts
+++ frontend/src/api/users/mutations.ts
@@ -19,3 +19,22 @@ export const useLoginMutation = () => {
     }
   });
 };
+
+// Mutation for signing up a new user
+export const useSignupMutation = () => {
+  return useMutation({
+    mutationFn: (credentials: { name: string; username: string, email: string; }) =>
+      apiClient.post("/api/v1/users/start-register", credentials),
+    onSuccess: (data) => {
+      // Handle successful signup
+      console.log("Verification email sended:", data);
+      // Typically you would:
+      // 1. Display a success message
+      // 2. Redirect the user to the login page
+    },
+    onError: (error) => {
+      // Handle errors
+      console.error("Signup failed:", error);
+    }
+  });
+};
