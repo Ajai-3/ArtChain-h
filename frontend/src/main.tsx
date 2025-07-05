eeada07be2b6e3ae5f046967371ee3ad17e38e34
Ajai-3 <ajaipjayan@gmail.com>
2025-07-05 11:59:40 +0530

    fix: Error fixed addign QueryProwider in main component warap entire app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
+import { QueryProvider } from "./api/providers/QueryClient.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ErrorBoundary>
-      <App />
+      <QueryProvider>
+        <App />
+      </QueryProvider>
     </ErrorBoundary>
   </BrowserRouter>
 );
