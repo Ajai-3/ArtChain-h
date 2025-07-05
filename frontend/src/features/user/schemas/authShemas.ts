accec46fb2cdc492cd7e60bc4a80b3fd176827d3
Ajai-3 <ajaipjayan@gmail.com>
2025-07-05 11:40:20 +0530

    feat: sign up schema and forgotpassword schema added

--- frontend/src/features/user/schemas/authShemas.ts
+++ frontend/src/features/user/schemas/authShemas.ts
@@ -1,8 +1,25 @@
-import z from "zod";
+import { z } from "zod";
 
 export const LoginSchema = z.object({
-    identity: z.string().min(3, "Email or username is required.").max(20, "Email or username is too long."),
-    password: z.string().min(8, "Password is required.").max(18, "Password is too long."),
-})
+  identifier: z.string().min(1, "Email or username is required"),
+  password: z.string().min(1, "Password is required"),
+});
+
+export const SignupSchema = z.object({
+  name: z.string().min(3, "Name is too short"),
+  username: z
+    .string()
+    .min(1, "Username is required")
+    .min(3, "Too short")
+    .regex(/^[a-zA-Z0-9_]+$/, "Use letters, numbers, or _ only"),
+  email: z.string().email("Enter a valid email"),
+});
+
+
+export const ForgotPasswordSchema = z.object({
+  identifier: z.string().min(3, "Email or username is required"),
+});
 
 export type LoginFormInputs = z.infer<typeof LoginSchema>;
+export type SignupFormInputs = z.infer<typeof SignupSchema>;
+export type ForgotPasswordFormInputs = z.infer<typeof ForgotPasswordSchema>;
