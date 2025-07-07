355c2718b471392a47fef629c9ce9bbe9d88c27c
Ajai-3 <ajaipjayan@gmail.com>
2025-07-07 18:24:28 +0530

    reset password ue case completed

--- services/user-service/src/2-application/user/ResetPasswordUseCase.ts
+++ services/user-service/src/2-application/user/ResetPasswordUseCase.ts
@@ -1,20 +1,25 @@
-import bcrypt from "bcrypt"
-import { NotFoundError } from "../../errors";
+import bcrypt from "bcrypt";
+import { BadRequestError, NotFoundError } from "../../errors";
 import { IUserRepository } from "../../1-domine/repositories/IUserRepositories";
 import { ERROR_MESSAGES } from "../../constants/errorMessages";
 
 export class ResetPasswordUseCase {
-    constructor(private userRepo: IUserRepository) {}
+  constructor(private userRepo: IUserRepository) {}
 
-    async execute (identifier: string, newPassword: string) {
-        const user = await this.userRepo.findOneByEmail(identifier);
-        if (!user) {
-            throw new NotFoundError(ERROR_MESSAGES.USER_NOT_FOUND)
-        }
+  async execute(identifier: string, newPassword: string) {
+    const user = await this.userRepo.findOneByEmail(identifier);
+    if (!user) {
+      throw new NotFoundError(ERROR_MESSAGES.USER_NOT_FOUND);
+    }
 
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-        user.password = hashedPassword;
-        
-        await this.userRepo.save(user);
-    }    
+    const isSamePassword = bcrypt.compareSync(newPassword, user.password);
+    if (isSamePassword) {
+      throw new BadRequestError(ERROR_MESSAGES.NEW_PASSWORD_IS_SAME_AS_CURRENT);
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+
+    await this.userRepo.save(user);
+  }
 }
