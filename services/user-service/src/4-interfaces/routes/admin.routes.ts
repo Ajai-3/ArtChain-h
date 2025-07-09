4ebd7843c2a8716f9c69a060ba1b20e6b78527dd
Ajai-3 <ajaipjayan@gmail.com>
2025-07-09 19:48:24 +0530

    Admin auth middleware added in the route

--- services/user-service/src/4-interfaces/routes/admin.routes.ts
+++ services/user-service/src/4-interfaces/routes/admin.routes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { loginAdmin, logoutAdmin } from '../controllers/admin/AuthAdminController';
 import { changeUserStatus, getAllUsers } from '../controllers/admin/UserMangementAdminController';
+import { AdminAuthMiddleware } from '../middlewares/AdminAuthMiddleware';
 const router = express.Router();
 
 router.post("/login", loginAdmin)
 router.post("/logout", logoutAdmin)
 
 //# User mangement
-router.get("/users", getAllUsers)
+router.get("/users", AdminAuthMiddleware, getAllUsers)
 router.post("/users/:userId/status", changeUserStatus)
 
 export default router;
