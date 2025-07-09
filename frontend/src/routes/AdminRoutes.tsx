12e8f42332247987f2d59c091492a6a88023c201
Ajai-3 <ajaipjayan@gmail.com>
2025-07-09 21:48:12 +0530

    admin routees chaged

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -1,15 +1,19 @@
-import { Route } from "react-router-dom";
 import AdminLayout from "../layouts/AdminLayout";
+import { Route, Navigate } from "react-router-dom";
 import Dashboard from "../features/admin/pages/Dashboard";
 import { Login } from "../features/admin/components/auth/Login";
-import UserMangement from "../features/admin/pages/UserMangement";
+import UserManagement from "../features/admin/pages/UserMangement";
 
 const AdminRoutes = [
   <Route path="/admin/login" element={<Login />} />,
-
-  <Route path="/admin/dashboard" element={<AdminLayout />}>
-    <Route index element={<Dashboard />} />
-    <Route path="user-management" element={<UserMangement />} />
+  
+  <Route path="/admin" element={<AdminLayout />}>
+    {/* Auto-redirect from /admin to /admin/dashboard */}
+    <Route index element={<Navigate to="dashboard" replace />} />
+    
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="user-management" element={<UserManagement />} />
+    {/* Other admin routes... */}
   </Route>,
 ];
 
