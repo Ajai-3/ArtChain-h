f35224d97869069b1d7f4fb8addcf845d6f563c6
Ajai-3 <ajaipjayan@gmail.com>
2025-07-11 15:46:22 +0530

    refactor: remove HelmetProvider from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { HelmetProvider } from "react-helmet-async";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryProvider } from "./api/providers/QueryClient.tsx";
 import App from "./App.tsx";
@@ -18,14 +17,12 @@ createRoot(document.getElementById("root")!).render(
         <Provider store={store}>
           <PersistGate persistor={persistor}>
             <ThemeProvider>
-              <HelmetProvider>
                 <App />
                 {import.meta.env.DEV && (
                   <ReactQueryDevtools
                     initialIsOpen={false}
                   />
                 )}
-              </HelmetProvider>
             </ThemeProvider>
           </PersistGate>
         </Provider>
