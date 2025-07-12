751f2cd25516c943d16ac2fa8eae9705d90035d6
Ajai-3 <ajaipjayan@gmail.com>
2025-07-12 18:08:13 +0530

    feat: add Artwork entity for managing artwork details and properties

---
+++ services/art-service/src/1-domine/entities/artwork.entity.ts
@@ -0,0 +1,19 @@
+export class Artwork {
+  constructor(
+    public _id: string,
+    public userId: string,
+    public title: string,
+    public description: string = '',
+    public tags: string[] = [],
+    public images: string[] = [],
+    public nsfw: boolean = false,
+    public isCollectible: boolean = false,
+    public artType: string = 'digital',
+    public price: number = 0,
+    public soldCopies: number = 0,
+    public downloadAccess: boolean = false,
+    public isCommentEnabled: boolean = true,
+    public createdAt: Date = new Date(),
+    public updatedAt: Date = new Date()
+  ) {}
+}
